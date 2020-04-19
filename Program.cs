381f70fc8e314214bed3dffbd896dd00f743d568
hubhub85 <czachorhubert@gmail.com>
2020-04-19 12:44:14 +0200

    zatw

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace kolko_i_krzyzyk
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string imieGraczaA = "";
+            string imieGraczaB = "";
+            char znakGraczaA = 'x';
+            char znakGraczaB = 'o';
+            char[,] plansza = new char[3, 3]
+            {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+            };
+
+            Console.Write("Wprowadź imię gracza A: ");
+            imieGraczaA = Console.ReadLine();
+            Console.Write("Wprowadź imię gracza B: ");
+            imieGraczaB = Console.ReadLine();
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
