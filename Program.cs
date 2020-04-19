7bd248b89b969e61b03f26f1fdf9664a72fa28de
hubhub85 <czachorhubert@gmail.com>
2020-04-19 14:30:35 +0200

    dodanie rysowania oraz jesen bład wystepuje napr!

--- Program.cs
+++ Program.cs
@@ -16,15 +16,45 @@ namespace kolko_i_krzyzyk
                 {'4', '5', '6'},
                 {'7', '8', '9'}
             };
+            char[,] planszaKopia = plansza.Clone() as char[,];
 
             Console.Write("Wprowadź imię gracza A: ");
             imieGraczaA = Console.ReadLine();
             Console.Write("Wprowadź imię gracza B: ");
             imieGraczaB = Console.ReadLine();
 
-            Console.ReadKey();
+            //Petla na kolejne ruchy graczy
+            bool koniceGry = false;
+            for (int runda = 0; runda < plansza.Length; ++runda)
+            {
+                Console.Clear();
+                RysujPlansze(plansza);
+
+                Console.ReadKey();
+
+                
+            }
+            
+
+
 
         }
 
+        static void RysujPlansze (char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+
+            for(int i= 0; i< wysokosc; ++i)
+            {
+                for (int j = 0; j < szerokosc; ++j)
+                {
+                    Console.Write(plansza[i, j]);
+                }
+                    
+            }
+                Console.ReadKey();
+                
+        }
     }
 }
